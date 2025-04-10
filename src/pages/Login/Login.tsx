8026f8e1f50037326b586ecdc9b54b3726451b53
adrianhenrique17 <hs8953064@gmail.com>
2025-04-10 02:15:31 -0300

    enviando atualização nas telas de login e senha e arruamando bugs

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -4,7 +4,6 @@ import logo from "../../assets/devwearball.png";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import api from "../../api/api";
-//html
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -20,17 +19,11 @@ const Login = () => {
       if (response.data.success) {
         console.log("Login bem-sucedido!");
 
-        // Armazena o token no localStorage
         localStorage.setItem("authToken", response.data.token);
-
-        // Armazena o ID do usuário no localStorage
         localStorage.setItem("userId", response.data.user.id);
 
-        // Redireciona para a tela principal
         window.location.href = "/TelaPrincipal";
       }
-
-      //tratativa de erro da api
     } catch (error) {
       if (axios.isAxiosError(error)) {
         if (error.response) {
@@ -48,71 +41,65 @@ const Login = () => {
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault(); //não deixa o botão de acessar, recarregar senão o form não envia
+    event.preventDefault();
 
-    //  e-mail
     if (!emailRegex.test(email)) {
       setError("Por favor, insira um e-mail válido.");
       return;
     }
 
     setError("");
-    console.log("Envio bem-sucedido!");
     ExecuteLogin();
   };
 
   return (
-    <div className="row">
-      <div className="d-flex justify-content-center align-items-center text-center">
-        {/* formatação com bootstrap */}
-        {/*col deixa em colunas o componente para ser mais responsivel */}
-        <form onSubmit={handleSubmit} className="form-signin">
-          <img src={logo} className="Logo" alt="DevWear Logo" />
-          <h6 className="mb-5">
-            <p>
-              <strong>{"<DevWear/>"}</strong>
-            </p>
-            <p className="texto-login">
-              {"Debugue seu estilo, compile sua identidade!"}
-            </p>
-          </h6>
-          <h4 className="mb-3 text-secondary">Acesse sua conta</h4>
+    <div className="login-page">
+      <div className="row-login">
+        <div className="d-flex justify-content-center align-items-center text-center">
+          <form onSubmit={handleSubmit} className="form-signin">
+            <img src={logo} className="Logo" alt="DevWear Logo" />
+            <h6 className="mb-5">
+              <p>
+                <strong>{"<DevWear/>"}</strong>
+              </p>
+              <p className="texto-login">
+                {"Debugue seu estilo, compile sua identidade!"}
+              </p>
+            </h6>
+            <h4 className="mb-3 text-secondary">Acesse sua conta</h4>
 
-          {/* Exibe a mensagem de erro caso o e-mail seja inválido */}
-          {error && <span className="text-danger">{error}</span>}
+            {error && <span className="text-danger">{error}</span>}
 
-          <div className="mt-1">
-            <input
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="form-control bg-secondary bg-opacity-25 text-dark"
-            />
-            {/* form control deixa é do Bs e deixa input redondo*/}
-          </div>
+            <div className="mt-1">
+              <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="form-control bg-secondary bg-opacity-25 text-dark"
+              />
+            </div>
 
-          <div className="mt-2">
-            <input
-              type="password"
-              placeholder="Senha"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="form-control bg-secondary bg-opacity-25 text-dark"
-            />
-          </div>
+            <div className="mt-2">
+              <input
+                type="password"
+                placeholder="Senha"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="form-control bg-secondary bg-opacity-25 text-dark"
+              />
+            </div>
 
-          <div className="mt-4">
-            <button className="btn btn-primary">Acessar</button>
-          </div>
+            <div className="mt-4">
+              <button className="btn btn-primary">Acessar</button>
+            </div>
 
-          <div className="mt-3">
-            <span>Registre-se. </span>
-            <Link to="/register">Aqui</Link>
-
-            {/* registrando na tela de login e Link e a tag do router que leva para as urls escolhidas*/}
-          </div>
-        </form>
+            <div className="mt-3">
+              <span>Registre-se. </span>
+              <Link to="/register">Aqui</Link>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -133,126 +133,122 @@ const Register = () => {
   };
 
   return (
-    <div className="row2">
-      <div className="d-flex justify-content-center align-items-center text-center">
-        {error && <div className="alert alert-danger">{error}</div>}
-        <form onSubmit={handleSubmit} className="form-signin2">
-          <img src={logo2} className="Logo2" alt="Logo DevWear" />
-          <h6 className="mb-7">
-            <p>
-              <strong>{"<DevWear/>"}</strong>
-            </p>
-          </h6>
-          <h6 className="mb-3 text-secondary">Registre sua conta aqui!</h6>
+    <div className="login-page">
+      <div className="row-login">
+        <div className="d-flex justify-content-center align-items-center text-center">
+          {error && <div className="alert alert-danger">{error}</div>}
+          <form onSubmit={handleSubmit} className="form-signin">
+            <img src={logo2} className="Logo" alt="Logo DevWear" />
+            <h6 className="mb-7">
+              <p>
+                <strong>{"<DevWear/>"}</strong>
+              </p>
+            </h6>
+            <h6 className="mb-3 text-secondary">Registre sua conta aqui!</h6>
 
-          <div className="mt-3">
-            <input
-              type="text"
-              name="name"
-              placeholder="Nome"
-              value={formData.name}
-              onChange={handleChange}
-              className={`form-control bg-secondary bg-opacity-25 text-dark ${
-                errors.name ? "is-invalid" : ""
-              }`}
-            />
-            {errors.name && (
-              <div className="invalid-feedback d-block text-start">
-                {errors.name}
-              </div>
-            )}
-          </div>
-
-          <div className="mt-3">
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              value={formData.email}
-              onChange={handleChange}
-              className={`form-control bg-secondary bg-opacity-25 text-dark ${
-                errors.email ? "is-invalid" : ""
-              }`}
-            />
-            {errors.email && (
-              <div className="invalid-feedback d-block text-start">
-                {errors.email}
-              </div>
-            )}
-          </div>
-
-          <div className="mt-3">
-            <input
-              type="password"
-              name="password"
-              placeholder="Senha"
-              value={formData.password}
-              onChange={handleChange}
-              className={`form-control bg-secondary bg-opacity-25 text-dark ${
-                errors.password ? "is-invalid" : ""
-              }`}
-            />
-            {errors.password && (
-              <div className="invalid-feedback d-block text-start">
-                {errors.password}
-              </div>
-            )}
-          </div>
-
-          <div className="mt-3">
-            <input
-              type="password"
-              name="confirmPassword"
-              placeholder="Confirme sua senha"
-              value={formData.confirmPassword}
-              onChange={handleChange}
-              className={`form-control bg-secondary bg-opacity-25 text-dark ${
-                errors.confirmPassword ? "is-invalid" : ""
-              }`}
-            />
-            {errors.confirmPassword && (
-              <div className="invalid-feedback d-block text-start">
-                {errors.confirmPassword}
-              </div>
-            )}
-          </div>
-
-          <div className="mt-3">
-            <InputMask
-              mask="999.999.999-99"
-              value={formData.cpf}
-              onChange={handleChange}
-            >
-              {(inputProps: React.InputHTMLAttributes<HTMLInputElement>) => (
-                <input
-                  {...inputProps}
-                  type="text"
-                  name="cpf"
-                  placeholder="CPF (xxx.xxx.xxx-xx)"
-                  className={`form-control bg-secondary bg-opacity-25 text-dark ${
-                    errors.cpf ? "is-invalid" : ""
-                  }`}
-                />
+            <div className="mt-3">
+              <input
+                type="text"
+                name="name"
+                placeholder="Nome"
+                value={formData.name}
+                onChange={handleChange}
+                className={`form-control ${errors.name ? "is-invalid" : ""}`}
+              />
+              {errors.name && (
+                <div className="invalid-feedback d-block text-start">
+                  {errors.name}
+                </div>
               )}
-            </InputMask>
-            {errors.cpf && (
-              <div className="invalid-feedback d-block text-start">
-                {errors.cpf}
-              </div>
-            )}
-          </div>
+            </div>
 
-          <div className="mt-4">
-            <button type="submit" className="btn btn-primary w-100">
-              Registrar
-            </button>
-          </div>
+            <div className="mt-3">
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={formData.email}
+                onChange={handleChange}
+                className={`form-control ${errors.email ? "is-invalid" : ""}`}
+              />
+              {errors.email && (
+                <div className="invalid-feedback d-block text-start">
+                  {errors.email}
+                </div>
+              )}
+            </div>
 
-          <div className="mt-1">
-            <span>Já tem uma conta? </span>
-            <Link to="/">Logue aqui!</Link>
-          </div>
-        </form>
+            <div className="mt-3">
+              <input
+                type="password"
+                name="password"
+                placeholder="Senha"
+                value={formData.password}
+                onChange={handleChange}
+                className={`form-control ${
+                  errors.password ? "is-invalid" : ""
+                }`}
+              />
+              {errors.password && (
+                <div className="invalid-feedback d-block text-start">
+                  {errors.password}
+                </div>
+              )}
+            </div>
+
+            <div className="mt-3">
+              <input
+                type="password"
+                name="confirmPassword"
+                placeholder="Confirme sua senha"
+                value={formData.confirmPassword}
+                onChange={handleChange}
+                className={`form-control ${
+                  errors.confirmPassword ? "is-invalid" : ""
+                }`}
+              />
+              {errors.confirmPassword && (
+                <div className="invalid-feedback d-block text-start">
+                  {errors.confirmPassword}
+                </div>
+              )}
+            </div>
+
+            <div className="mt-3">
+              <InputMask
+                mask="999.999.999-99"
+                value={formData.cpf}
+                onChange={handleChange}
+              >
+                {(inputProps: React.InputHTMLAttributes<HTMLInputElement>) => (
+                  <input
+                    {...inputProps}
+                    type="text"
+                    name="cpf"
+                    placeholder="CPF (xxx.xxx.xxx-xx)"
+                    className={`form-control ${errors.cpf ? "is-invalid" : ""}`}
+                  />
+                )}
+              </InputMask>
+              {errors.cpf && (
+                <div className="invalid-feedback d-block text-start">
+                  {errors.cpf}
+                </div>
+              )}
+            </div>
+
+            <div className="mt-4">
+              <button type="submit" className="btn btn-primary w-100">
+                Registrar
+              </button>
+            </div>
+
+            <div className="links">
+              <span>Já tem uma conta? </span>
+              <Link to="/"> Logue aqui!</Link>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
