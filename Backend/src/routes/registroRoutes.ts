7e221185784d5882879f55f12a0900c951d83532
Adrian Henrique Olinek da Silva <162664330+adrianhenrique17@users.noreply.github.com>
2025-04-24 21:00:39 -0300

    Delete Backend directory

--- Backend/src/routes/registroRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getAll } from "../controllers/registroController";
-
-const router = express.Router();
-
-router.get("/registros", getAll);
-
-export default router
